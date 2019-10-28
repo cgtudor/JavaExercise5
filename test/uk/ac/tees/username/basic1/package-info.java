768220ef7623d28e53890ef5c299de99ecde1303
gitGudOrGitOut <47678302+gitGudOrGitOut@users.noreply.github.com>
2019-10-28 14:31:35 +0000

    Initial commit

---
+++ test/uk/ac/tees/username/basic1/package-info.java
@@ -0,0 +1,6 @@
+
+/** <p>A set of unit tests to check the solution to the BASIC1 task.</p>
+ *
+ * Based on work created by Mark Truran </p>
+ */
+package uk.ac.tees.username.basic1;
---
+++ test/uk/ac/tees/username/standard/package-info.java
@@ -0,0 +1,5 @@
+/** <p>A set of unit tests to check the solution to the STANDARD task.</p>
+ *
+ * Based on work created by Mark Truran </p>
+ */
+package uk.ac.tees.username.standard;
